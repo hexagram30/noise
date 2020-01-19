0e9a74776b2488b1d693dfde94e8d606031bc6f3
Duncan McGreggor <oubiwann@gmail.com>
2020-01-19 13:24:47 -0600

    Added builder abstraction.

--- src/gen/caves.rs
+++ src/gen/caves.rs
@@ -1,91 +1,55 @@
 extern crate noise;
 
-use noise::utils::*;
 use noise::{Billow, Fbm, HybridMulti, Perlin, RidgedMulti, Turbulence};
+use crate::base::{Builder, Opts};
 use crate::modifiers::{Invert, Threshold};
 
-#[derive(Clone, Copy, Default)]
-pub struct Resolution {
-    pub x: usize,
-    pub y: usize,
-}
-
-#[derive(Default)]
-pub struct Opts {
-    pub inverted: bool,
-    pub output: String,
-    pub seed: i64,
-    pub res: Resolution,
-    pub res_str: String,
-    pub threshold_cutoff: f64,
-    pub tiled: bool,
-    pub turbulence: bool,
-}
-
-pub fn complex(opts: Opts) {
+pub fn complex(opts: &Opts) {
     let generator = Billow::new();
     let generator = Threshold::new(&generator)
         .set_cutoff(opts.threshold_cutoff);
-    PlaneMapBuilder::new(&generator)
-        .set_size(opts.res.x, opts.res.y)
-        .set_is_seamless(opts.tiled)
-        .build()
-        .write_to_file(&opts.output);
+    let builder = Builder::new(&generator, opts);
+    builder.generate()
 }
 
-pub fn fractured(opts: Opts) {
+pub fn fractured(opts: &Opts) {
     let generator = HybridMulti::new();
     let generator = Threshold::new(&generator)
         .set_cutoff(opts.threshold_cutoff);
-    PlaneMapBuilder::new(&generator)
-        .set_size(opts.res.x, opts.res.y)
-        .set_is_seamless(opts.tiled)
-        .build()
-        .write_to_file(&opts.output);
+    let builder = Builder::new(&generator, opts);
+    builder.generate()
 }
 
-pub fn jagged_walls(opts: Opts) {
+pub fn jagged_walls(opts: &Opts) {
     let generator = Fbm::new();
     let generator = Threshold::new(&generator)
         .set_cutoff(opts.threshold_cutoff);
-    PlaneMapBuilder::new(&generator)
-        .set_size(opts.res.x, opts.res.y)
-        .set_is_seamless(opts.tiled)
-        .build()
-        .write_to_file(&opts.output);
+    let builder = Builder::new(&generator, opts);
+    builder.generate()
 }
 
-pub fn linear(opts: Opts) {
+pub fn linear(opts: &Opts) {
     let generator = RidgedMulti::new();
     let generator = Threshold::new(&generator)
         .set_cutoff(opts.threshold_cutoff);
     let generator = Invert::new(&generator);
-    PlaneMapBuilder::new(&generator)
-        .set_size(opts.res.x, opts.res.y)
-        .set_is_seamless(opts.tiled)
-        .build()
-        .write_to_file(&opts.output);
+    let builder = Builder::new(&generator, opts);
+    builder.generate()
 }
 
-pub fn simple(opts: Opts) {
+pub fn simple(opts: &Opts) {
     let generator = Perlin::new();
     let generator = Threshold::new(&generator)
         .set_cutoff(opts.threshold_cutoff);
-    PlaneMapBuilder::new(&generator)
-        .set_size(opts.res.x, opts.res.y)
-        .set_is_seamless(opts.tiled)
-        .build()
-        .write_to_file(&opts.output);
+    let builder = Builder::new(&generator, opts);
+    builder.generate()
 }
 
-pub fn wobbly_walls(opts: Opts) {
+pub fn wobbly_walls(opts: &Opts) {
     let generator = Perlin::new();
     let generator = Turbulence::new(&generator);
     let generator = Threshold::new(&generator)
         .set_cutoff(opts.threshold_cutoff);
-    PlaneMapBuilder::new(&generator)
-        .set_size(opts.res.x, opts.res.y)
-        .set_is_seamless(opts.tiled)
-        .build()
-        .write_to_file(&opts.output);
+    let builder = Builder::new(&generator, opts);
+    builder.generate()
 }
