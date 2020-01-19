d0ca185f738f6c17d7f296abdc43e8d1e3173c5a
Duncan McGreggor <oubiwann@gmail.com>
2020-01-19 00:47:28 -0600

    Updated examples to use the new cave functions.

--- examples/caves.rs
+++ examples/caves.rs
@@ -1,59 +1,50 @@
 extern crate noise;
 
-use noise::utils::*;
-use noise::{Billow, Fbm, HybridMulti, Perlin, RidgedMulti, Turbulence};
-use hxgm30noise::modifiers::{Invert, Threshold};
+use hxgm30noise::gen::caves;
+use hxgm30noise::gen::caves::{Opts, Size};
 
 fn main() {
-    let rm = RidgedMulti::new();
-    let thresh = Threshold::new(&rm)
-        .set_cutoff(-0.4);
-    let invert = Invert::new(&thresh);
 
-    PlaneMapBuilder::new(&invert)
-        .set_size(500, 500)
-        .build()
-        .write_to_file("caves-rm.png");
+    let size = Size{x: 500, y: 500};
 
-    let billow = Billow::new();
-    let thresh = Threshold::new(&billow)
-        .set_cutoff(-0.25);
+    caves::complex(Opts{
+        output: "caves-complex-billow.png".to_string(),
+        threshold_cutoff: -0.25,
+        inverted: true,
+        size,
+        .. Default::default()
+    });
 
-    PlaneMapBuilder::new(&thresh)
-        .set_size(500, 500)
-        .build()
-        .write_to_file("caves-billow.png");
+    caves::fractured(Opts{
+        output: "caves-fractured-hm.png".to_string(),
+        threshold_cutoff: 0.1,
+        size,
+        .. Default::default()
+    });
 
-    let perlin = Perlin::new();
-    let thresh = Threshold::new(&perlin);
+    caves::jagged_walls(Opts{
+        output: "caves-jagged-walls-fbm.png".to_string(),
+        size,
+        .. Default::default()
+    });
 
-    PlaneMapBuilder::new(&thresh)
-        .set_size(500, 500)
-        .build()
-        .write_to_file("caves-perlin.png");
+    caves::linear(Opts{
+        output: "caves-linear-rm.png".to_string(),
+        threshold_cutoff: -0.4,
+        size,
+        .. Default::default()
+    });
 
-    let fbm = Fbm::new();
-    let thresh = Threshold::new(&fbm);
+    caves::simple(Opts{
+        output: "caves-simple-perlin.png".to_string(),
+        size,
+        .. Default::default()
+    });
 
-    PlaneMapBuilder::new(&thresh)
-        .set_size(500, 500)
-        .build()
-        .write_to_file("caves-fbm.png");
+   caves::wobbly_walls(Opts{
+        output: "caves-wobbly-walls-turbulence.png".to_string(),
+        size,
+        .. Default::default()
+    });
 
-    let hm = HybridMulti::new();
-    let thresh = Threshold::new(&hm);
-
-    PlaneMapBuilder::new(&thresh)
-        .set_size(500, 500)
-        .build()
-        .write_to_file("caves-hm.png");
-
-    let perlin = Perlin::new();
-    let t = Turbulence::new(&perlin);
-    let thresh = Threshold::new(&t);
-
-    PlaneMapBuilder::new(&thresh)
-        .set_size(500, 500)
-        .build()
-        .write_to_file("caves-turbulence.png");
 }
